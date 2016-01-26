a882a4a66bb41d09d62d12657626cb235492b21c
luiscaserta <luiscaserta@yahoo.com>
2016-01-26 14:37:27 -0500

    Stage 2 - Second Submission
    Implemented all updates to second submission based on initial feedback
    and further validation update to function level (easy, medium, hard)

--- Second_Submission_IPND_Stage_2_Project.py
+++ Second_Submission_IPND_Stage_2_Project.py
@@ -37,77 +37,66 @@ hard_answers = ["anything", "mutation", "square brackets", "aliasing", ]
 blanks = ["__1__", "__2__", "__3__", "__4__"]
 
 def number_of_tries(tries):
-  '''This is the function to determine the number of guesses the user want to make for each anser.
-  input is a user defined integer
-  output is the user defined integer'''
-  #This is my code to validate the input of the user.  It only accepts a positive integer.  I thought of this because I kept getting
-  #an occasional error after I was asking for number of times the user wanted to guess the correct answer.  I didn't panic, and "eventually"
-  #I was able to figure it out.  I figured this out by searching on the internet, and I am most proud of this little bit of code!! =)
+  '''This is the function to determine the number of guesses the user want to make for each answer.
+  Input is a user defined integer. I include validation to make sure that is what is provided.
+  Output is the user defined integer accepted only as a positive integer.'''
   while True:
     try:
-      tries = int(raw_input("How many guesses would you like for each problem?  Please enter a positive integer: "))
+      tries = int(raw_input("How many guesses would you like for each problem?  Please enter a positive integer: \n"))
     except ValueError:
-      print "Sorry, I didn't understand. Input must be a positive integer"
+      print "Sorry, I didn't understand. Input must be a positive integer\n"
       continue
     if tries <= 0:
-      print "Sorry, your response must not be negative!!"
+      print "Sorry, your response must not be negative!!\n"
       continue
     else:
       break
   return tries
 
-
-
 def game_level(level):
-  '''This is my function to have the user determine what level he wants to play.  I use the user's raw input answer to select the
-  corresponding test and answer set.
-  inputs are.....
-  outputs are....'''
-  input = raw_input("Choose level of game to play (easy, medium, hard): ")
-  print "You have chosen level " + input + "!!"
-  
-  number_of_tries(raw_input)
-  
+  '''This is my function to have the user determine what level he wants to play.  I validate user input. 
+  I use the user's raw input answer to select the corresponding test and answer set.
+  Input is a user defined string (easy, medium, or hard).
+  Outputs are the corresponding variables associated with the defined level.'''
+  input = raw_input("Choose level of game to play (easy, medium, hard): \n")
+  if not input in ["easy","Easy", "medium", "Medium", "hard", "Hard"]:
+    print "That is not a valid option!!\n"
+    return game_level(level)
+  print "You have chosen level " + input + "!!\n"
+  tries = number_of_tries(raw_input)
   if input in ["easy","Easy"]: 
     play_game(easy_test, easy_answers, blanks, tries)
   elif input in["medium", "Medium"]:
     play_game(medium_test, medium_answers, blanks, tries)
   elif input in ["hard", "Hard"]:
     play_game(hard_test, hard_answers, blanks, tries)
-  else:
-    print "That is not a valid option!!"
-    return game_level(level)
+
 
 def play_game (test, answer, blank, tries):
-  '''This is my function using simple while loop that sets the conditions for user to answer the fill in the blank test in accordance their own criteria.
+  '''This is my function using a simple while loop that sets the conditions for user to answer the fill in the blank test in accordance their own criteria.
   The variable i represents the blanks that must be filled in each test.  Once all blanks filled correctly, while loop ends.
   Chances is the variable that starts at 0 and must be less than tries to allow the game to continue.  If equal to user input tries
   it will end the game according to the instructions.
-  inputs are......
-  outputs are..........'''
+  Inputs are test, answer, blank and user defined number of tries.
+  Outputs are the text associated with the level, the fill in the blanks for each, and feedback to the user until the game is ended.'''
   i = 0
   chances = 0
-
-  print test
-
+  print test + "\n"
   while (i < len(answer)) and (chances < tries):
-    guess = raw_input("What is your correct answer for " + blank[i] + "? ")
+    guess = raw_input("What is your correct answer for " + blank[i] + "? \n")
     if guess == answer[i]:
-      print "That is correct!!"
-      print
+      print "That is correct!!\n"
       test = test.replace(blank[i], answer[i])
       print test
-      print
       i = i + 1
       if i == len(answer):
-        print "Nice work! You've correctly filled in the blanks and won the game!"
+        print "Nice work! You've correctly filled in the blanks and won the game!\n"
     else:
-       chances = chances + 1
-       if chances == tries:
-         print "Sorry! You did not provide the correct answer. Thanks for playing."
-       else:
-         print "Sorry - that is incorrect. Please try again!!"
-         print
+      chances = chances + 1
+      if chances == tries:
+        print "Sorry! You did not provide the correct answer. Thanks for playing.\n"
+      else:
+        print "Sorry - that is incorrect. Please try again!!\n"
 
 #This is how I start the game
 game_level(raw_input)
